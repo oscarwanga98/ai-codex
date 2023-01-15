34b7d07d63e89e7c51e3f8954e46da25e649f3ff
oscarwanga98 <87162511+oscarwanga98@users.noreply.github.com>
2023-01-15 14:16:52 +0300

    Update script.js

--- client/script.js
+++ client/script.js
@@ -78,7 +78,7 @@ function chatStripe(isAi,value,uniqueId){
   loader(messageDiv);
 
   //fetch data from server
-  const response =await fetch ('http://localhost:5000',{
+  const response =await fetch ('https://aiengine.onrender.com/',{
     method:'POST',
     headers:{
       "Content-Type":"application/json"
